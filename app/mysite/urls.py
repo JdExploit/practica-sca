0ab6530972c47ad06ef9b25c750c8d01ba73df77
JDEXPLOIT <100084237+JdExploit@users.noreply.github.com>
2026-02-17 00:42:34 +0100

    Update urls.py

--- app/mysite/urls.py
+++ app/mysite/urls.py
@@ -1,7 +1,7 @@
-"""mysite URL Configuration"""
+"""mysite URL Configuration for Django 1.11"""
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
